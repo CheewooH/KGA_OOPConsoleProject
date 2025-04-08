4d0fd02cb2a7eccab1748ec61163fad6a3f9d789
홍치우 <1974cldn@gmail.com>
2025-04-08 11:40:33 +0900

    (feat) 캐릭터 디테일 추가
    죽음의 문턱, 스트레스, 크리티컬 구조 수정,

--- KGA_OOPConsoleProject/KGA_OOPConsoleProject/Character.cs
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Character.cs
@@ -3,39 +3,75 @@
     public class Character
     {
         // HP와 스트레스만 가지고 나머지 스탯은 장비로
-        public string name { get; set; }
-        private int hp;
+        public string Name { get; set; }
+        private int Hp;
         public int HP
         {
-            get { return hp; }
+            get { return Hp; }
             set
             {
-                hp = value;
-                if (hp < 0)
+                Hp = value;
+                if (Hp < 0)
                 {
-                    hp = 0;
+                    Hp = 0;
                 }
             }
         }
-        public int stress {  get; set; }
-        public double dodge { get; set; }
+        public int Stress { get; set; }
+        public double Dodge { get; set; }
+        public double Critical { get; set; }
+
+        private static Random random = new Random();
+        public Character(string name, int maxHp, int stress, double dodge = 0.1, double critical = 0.15)
+        {
+            Name = name;
+            Hp = maxHp;
+            Stress = stress;
+            Dodge = dodge;
+            Critical = critical;
+        }
         public int Cirtical(int damage, ref bool isCritical)
         {
-            int critical = new Random().Next(1, 100);
-            if (critical <= 15)
-            { // 크리티컬일때
+            if (random.NextDouble() < Critical)
+            {
                 isCritical = true;
-                // TODO
+                damage = (int)Math.Round(damage * 1.5);
+                Console.WriteLine($"크리티컬!");
             }
             else
             {
                 isCritical = false;
             }
-
             return damage;
-
         }
 
+        
+        public void TakeDamage(int damage)
+        {
+            if (random.NextDouble() < Dodge)
+            {
+                Console.WriteLine("회피했다!");
+                return;
+            }
+            Hp -= damage;
+            Console.WriteLine($"{damage}데미지를 받았다.");
+        }
+        public void AddStress(int stress)
+        {
+            Stress += stress;
+                if (stress > 100)
+            {
+                stress = 100;
+                Hp = 1;
+                Console.WriteLine($"{Name}의 스트레스 가 한계치에 도달했습니다!");
+                Console.WriteLine("죽음의 문턱");
+            }
+            Console.WriteLine($"{Name}의 스트레스 : {stress}");
+        }
+        public bool IsAlive()
+        {
+            return Hp > 0;
+        }
 
 
     }
