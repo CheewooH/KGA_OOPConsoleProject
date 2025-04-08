60dcb794c4f7ad686350554d74f7ab75a77b4fc1
홍치우 <1974cldn@gmail.com>
2025-04-08 10:41:16 +0900

    (feat) 기본클래스만들고 캐릭터 내부 채워넣기

---
+++ KGA_OOPConsoleProject/KGA_OOPConsoleProject/Character.cs
@@ -0,0 +1,42 @@
+﻿namespace KGA_OOPConsoleProject
+{
+    public class Character
+    {
+        // HP와 스트레스만 가지고 나머지 스탯은 장비로
+        public string name { get; set; }
+        private int hp;
+        public int HP
+        {
+            get { return hp; }
+            set
+            {
+                hp = value;
+                if (hp < 0)
+                {
+                    hp = 0;
+                }
+            }
+        }
+        public int stress {  get; set; }
+        public double dodge { get; set; }
+        public int Cirtical(int damage, ref bool isCritical)
+        {
+            int critical = new Random().Next(1, 100);
+            if (critical <= 15)
+            { // 크리티컬일때
+                isCritical = true;
+                // TODO
+            }
+            else
+            {
+                isCritical = false;
+            }
+
+            return damage;
+
+        }
+
+
+
+    }
+}
